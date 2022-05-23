64aedab9537c8fa9227fa2f05754a49c107d3699
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-05-23 10:48:32 +0300

    Linking the random image to the HTML file | JS

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+// DICE ONE
 // Returns a random integer from 1 to 6, for the first dice: 
 const diceOneRandomNum = Math.floor(Math.random() * 6) + 1;
-
 // Linking images to the generated numbers | For the first dice
 // by concatenating, diceOneImage becomes equal to assets/diceX.png where X is from 1 to 6
 const diceOneImage = 'assets/dice' + diceOneRandomNum + '.png';
+// Putting the generated random image into the img1 class.
+document.getElementsByClassName("img1")[0].setAttribute('src', diceOneImage);
 
+
+// DICE TWO
 // Returns a random integer from 1 to 6, for the first dice: 
 const diceTwoRandomNum = Math.floor(Math.random() * 6) + 1;
-
 // Linking images to the generated numbers | For the second dice
 const diceTwoImage = 'assets/dice' + diceTwoRandomNum + '.png';
+// Putting the generated random image into the img2 class.
+document.getElementsByClassName("img2")[0].setAttribute('src', diceTwoImage);
+
+
+// use this :triangular_flag_on_post: emoji 
