ecf10a9e077b08cf5951cf5eb561d757f1ec75d8
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-05-23 10:41:12 +0300

    Adding the . to .png so the path can be accesses | JS

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const diceOneRandomNum = Math.floor(Math.random() * 6) + 1;
 
 // Linking images to the generated numbers | For the first dice
 // by concatenating, diceOneImage becomes equal to assets/diceX.png where X is from 1 to 6
-const diceOneImage = 'assets/dice' + diceOneRandomNum + 'png';
+const diceOneImage = 'assets/dice' + diceOneRandomNum + '.png';
 
 // Returns a random integer from 1 to 6, for the first dice: 
 const diceTwoRandomNum = Math.floor(Math.random() * 6) + 1;
 
 // Linking images to the generated numbers | For the second dice
-const diceTwoImage = 'assets/dice' + diceTwoRandomNum + 'png';
+const diceTwoImage = 'assets/dice' + diceTwoRandomNum + '.png';
