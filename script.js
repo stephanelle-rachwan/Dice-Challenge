7989a37d36a19c1c228df50ae0eca043265026f1
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-05-23 10:39:23 +0300

    Creating a const with the same name as the images using the random numbers | JS

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 // Returns a random integer from 1 to 6, for the first dice: 
 const diceOneRandomNum = Math.floor(Math.random() * 6) + 1;
 
+// Linking images to the generated numbers | For the first dice
+// by concatenating, diceOneImage becomes equal to assets/diceX.png where X is from 1 to 6
+const diceOneImage = 'assets/dice' + diceOneRandomNum + 'png';
+
 // Returns a random integer from 1 to 6, for the first dice: 
 const diceTwoRandomNum = Math.floor(Math.random() * 6) + 1;
+
+// Linking images to the generated numbers | For the second dice
+const diceTwoImage = 'assets/dice' + diceTwoRandomNum + 'png';
