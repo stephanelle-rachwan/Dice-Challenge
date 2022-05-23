02d80618e4a201067e0eafcab5c7535b1a662fe1
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-05-23 10:30:47 +0300

    Adding the math.random fct to generate random numbers for both dices | JS

---
+++ script.js
@@ -0,0 +1,5 @@
+// Returns a random integer from 1 to 6, for the first dice: 
+const diceOneRandomNum = Math.floor(Math.random() * 6) + 1;
+
+// Returns a random integer from 1 to 6, for the first dice: 
+const diceTwoRandomNum = Math.floor(Math.random() * 6) + 1;
